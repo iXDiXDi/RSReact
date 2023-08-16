c2c644985ddbdd87eab591c3f6a0492bb3aef227
Dmitrii Karpenko <ixdixdi@gmail.com>
2023-08-16 17:00:28 +0500

    Rebuild "noJSX" format

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,46 @@
 //Императивный (получаем что то не важно как)
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
 	let currentDate = new Date();
 	let currentYear = currentDate.getFullYear();
 
 	return (
-		// Декларативная часть, хотя мы не знаем как браузер это всё рисует :-)
-		// Хотя видим что и как он должен сделать
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Что то новенькое.</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
-		//закончилась декларативная часть
+		// Декларативная часть, которая была в предидущей версии - тут отсутствует, как мне кажется.
+		createElement(
+			'div',
+			{
+				className: 'App',
+			},
+			createElement(
+				'header',
+				{
+					className: 'App-header',
+				},
+				createElement('img', {
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				}),
+				createElement(
+					'p',
+					null,
+					'\u0427\u0442\u043E \u0442\u043E \u043D\u043E\u0432\u0435\u043D\u044C\u043A\u043E\u0435.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('p', null, currentYear),
+			),
+		)
 	);
 };
